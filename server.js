fc8469a07cae25a0491e236c086581ebbe971066
clague17 <lclague9@gmail.com>
2019-02-24 00:29:23 -0600

    Second commit

--- server.js
+++ server.js
@@ -13,6 +13,5 @@ app.post('/', function (req, res) {
   console.log(req.body.city);
   res.render('index');
 })
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
+app.listen(process.env.PORT || 3000)
