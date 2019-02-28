08f6b20f3aa1ade41b21be3f27bf84236c7f1a8f
clague17 <lclague9@gmail.com>
2019-02-27 22:13:45 -0600

    fixed some bugs

--- public/js/logic.js
+++ public/js/logic.js
@@ -6,7 +6,7 @@ var createTeam = function() {
         //stress that this cannot be changed later.
 }
 
-var answers = {1: }
+// var answers = {1: }
 
 
 var team;
@@ -21,22 +21,22 @@ var displayCounter = 0;
 var provider = new firebase.auth.FacebookAuthProvider();
 provider.addScope(name);
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+// firebase.auth().signInWithPopup(provider).then(function(result) {
+//   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+//   var token = result.credential.accessToken;
+//   // The signed-in user info.
+//   var user = result.user;
+//   // ...
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
 
 // This is called with the results from from FB.getLoginStatus().
 var isLoggedIn = false;
